8ecbfc14002949dbefa4db974b506c81165fe65c
jongseo0703 <jongseo0703@naver.com>
2025-07-09 08:28:43 +0900

    ♻️ Refactor : 코드 스타일 양식에 맞게 코드 수정

--- src/main/java/com/sinse/hello_spring/controller/HelloController.java
+++ src/main/java/com/sinse/hello_spring/controller/HelloController.java
@@ -9,35 +9,31 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
 
-    @GetMapping("hello")
-    public String hello(Model model) {
-        model.addAttribute("data", "hello!!");
-        return "hello";
-    }
+  @GetMapping("hello")
+  public String hello(Model model) {
+    model.addAttribute("data", "hello!!");
+    return "hello";
+  }
 
-    @GetMapping("hello-mvc")
-    public String helloMvc(@RequestParam("name") String name, Model model) {
-        model.addAttribute("name", name);
-        return "hello-template";
-    }
+  @GetMapping("hello-mvc")
+  public String helloMvc(@RequestParam("name") String name, Model model) {
+    model.addAttribute("name", name);
+    return "hello-template";
+  }
 
-    /**
-     * ResponseBody는 데이터 자체만 전달함 html 자체를 전달하는 것과 다름
-     */
-    @GetMapping("hello-spring")
-    @ResponseBody
-    public String helloSpring(@RequestParam("name") String name) {
-        return "hello " + name;
-    }
+  /** ResponseBody는 데이터 자체만 전달함 html 자체를 전달하는 것과 다름 */
+  @GetMapping("hello-spring")
+  @ResponseBody
+  public String helloSpring(@RequestParam("name") String name) {
+    return "hello " + name;
+  }
 
-    /**
-     * 객체를 전달하면 JSON 형식으로 전달됨
-     */
-    @GetMapping("hello-api")
-    @ResponseBody
-    public Hello helloApi(@RequestParam("name") String name) {
-        Hello hello = new Hello();
-        hello.setName(name);
-        return hello;
-    }
+  /** 객체를 전달하면 JSON 형식으로 전달됨 */
+  @GetMapping("hello-api")
+  @ResponseBody
+  public Hello helloApi(@RequestParam("name") String name) {
+    Hello hello = new Hello();
+    hello.setName(name);
+    return hello;
+  }
 }
