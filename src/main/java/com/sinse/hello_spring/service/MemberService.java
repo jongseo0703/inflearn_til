a035bfb7f04603709b322c93d0dc75acce515109
jongseo0703 <jongseo0703@naver.com>
2025-07-09 14:00:49 +0900

    ✨ Feat : 회원 로직 관련 service 추가

---
+++ src/main/java/com/sinse/hello_spring/service/MemberService.java
@@ -0,0 +1,43 @@
+package com.sinse.hello_spring.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.sinse.hello_spring.domain.Member;
+import com.sinse.hello_spring.repository.MemberRepository;
+
+public class MemberService {
+
+  private final MemberRepository memberRepository;
+
+  public MemberService(MemberRepository memberRepository) {
+    this.memberRepository = memberRepository;
+  }
+
+  /** 회원가입 */
+  public long join(Member member) {
+    // 같은 이름이 있는 중복 회원X
+    validateDuplicateMember(member);
+
+    memberRepository.save(member);
+    return member.getId();
+  }
+
+  private void validateDuplicateMember(Member member) {
+    memberRepository
+        .findByName(member.getName())
+        .ifPresent(
+            m -> {
+              throw new IllegalStateException("이미 존재하는 회원입니다.");
+            });
+  }
+
+  /** 전체 회원 조회 */
+  public List<Member> findMembers() {
+    return memberRepository.findAll();
+  }
+
+  public Optional<Member> findOne(Long memberId) {
+    return memberRepository.findById(memberId);
+  }
+}
