4eefd548321e119d9f6d6ae5385b19afc50ae51c
jongseo0703 <jongseo0703@naver.com>
2025-07-10 17:45:35 +0900

    ✨ Feat : 회원 관리 예제

--- src/main/java/com/sinse/hello_spring/controller/MemberController.java
+++ src/main/java/com/sinse/hello_spring/controller/MemberController.java
@@ -1,8 +1,15 @@
 package com.sinse.hello_spring.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.sinse.hello_spring.domain.Member;
+import com.sinse.hello_spring.domain.MemberForm;
 import com.sinse.hello_spring.service.MemberService;
 
 @Controller
@@ -13,5 +20,29 @@ public class MemberController {
   @Autowired
   public MemberController(MemberService memberService) {
     this.memberService = memberService;
+  }
+
+  @GetMapping("/members/new")
+  public String createForm() {
+    return "members/createMemberForm";
+  }
+
+  @PostMapping("/members/new")
+  public String create(MemberForm memberForm) {
+    Member member = new Member();
+    member.setName(memberForm.getName());
+
+    memberService.join(member);
+
+    System.out.println(member.getName());
+
+    return "redirect:/";
+  }
+
+  @GetMapping("/members")
+  public String list(Model model) {
+    List<Member> members = memberService.findMembers();
+    model.addAttribute("members", members);
+    return "members/memberList";
   }
 }
---
+++ src/main/java/com/sinse/hello_spring/domain/MemberForm.java
@@ -0,0 +1,8 @@
+package com.sinse.hello_spring.domain;
+
+import lombok.Data;
+
+@Data
+public class MemberForm {
+  private String name;
+}
